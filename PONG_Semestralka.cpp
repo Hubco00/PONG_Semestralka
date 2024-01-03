d5251cc023c869a87070a9cf2f8a827f74f76f45
Hubco00 <adrianhubinak@gmail.com>
2024-01-03 20:34:11 +0100

    Add project files.

---
+++ PONG_Semestralka.cpp
@@ -0,0 +1,12 @@
+﻿// PONG_Semestralka.cpp : Defines the entry point for the application.
+//
+
+#include "PONG_Semestralka.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
